eeb5282df3be6a5fde1b87d0c201f49c7671cba6
Juraj Trappl <juraj.trappl@proton.me>
2023-08-01 12:54:38 +0200

    rename package name

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
-black oarepo_model_builder_relations tests --target-version py310
-autoflake --in-place --remove-all-unused-imports --recursive oarepo_model_builder_relations tests
-isort oarepo_model_builder_relations tests  --profile black
+black oarepo_published_service tests --target-version py310
+autoflake --in-place --remove-all-unused-imports --recursive oarepo_published_service tests
+isort oarepo_published_service tests  --profile black
