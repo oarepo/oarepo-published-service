25d7a8ae3f446e5f5a2a755477133b56f6401f59
Mirek Simek <miroslav.simek@gmail.com>
2023-12-14 14:00:25 +0100

    working on tests

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,6 @@
 set -e
 
 OAREPO_VERSION=${OAREPO_VERSION:-11}
-OAREPO_VERSION_MAX=$((OAREPO_VERSION+1))
 
 if [ -d .venv-builder ] ; then
     rm -rf .venv-builder
@@ -35,8 +34,7 @@ python3 -m venv .venv-tests
 source .venv-tests/bin/activate
 
 pip install -U setuptools pip wheel
-pip install "oarepo>=$OAREPO_VERSION,<$OAREPO_VERSION_MAX"
+pip install "oarepo[tests]==$OAREPO_VERSION.*"
 pip install -e "built_tests/model[tests]"
-pip install pytest-invenio
 
 pytest tests
